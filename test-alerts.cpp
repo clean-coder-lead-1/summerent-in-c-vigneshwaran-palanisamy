f0ffae4936f32eae46ba5de798439a6619f97f82
vigneshwaran-palanisamy <102678661+vigneshwaran-palanisamy@users.noreply.github.com>
2022-03-31 23:18:51 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,11 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(45, 30, 40) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(35, 27, 42) == NORMAL);
+}
